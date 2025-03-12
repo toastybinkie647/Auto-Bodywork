b8a3cb3b8665440da9607e561faee422fd2d785d
jono <jonathanries753@gmail.com>
2025-03-13 09:45:20 +1300

    Adding more css for the index page. working on animations

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-// JavaScript Document
+//let btn = document.querySelector("#btn");
+//let sidebar = document.querySelector(".sidebar");
+
+//btn.onclick = function () {
+	//sidebar.classList.toggle("active");
+//};
+
+function toggleSidebar() {
+	const sidebar = document.querySelector('.sidebar');
+	sidebar.classList.toggle('active');
+}
