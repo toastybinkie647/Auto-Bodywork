6ca98a373c2740f40c0bd8371aaf00a24c0dc162
jono <jonathanries753@gmail.com>
2025-04-06 10:41:28 +1200

    replacing placeholder imgs, finshed styling on main features, finsished animations

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
-//let btn = document.querySelector("#btn");
-//let sidebar = document.querySelector(".sidebar");
-
-//btn.onclick = function () {
-	//sidebar.classList.toggle("active");
-//};
 
 function toggleSidebar() {
 	const sidebar = document.querySelector('.sidebar');
+	const title = document.querySelector('.title');
 	sidebar.classList.toggle('active');
+	title.classList.toggle('active');
 }
 
 document.addEventListener("DOMContentLoaded", function() {
