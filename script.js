6cd732b00d85f038dbc13e6820a708d813812db9
jono <jonathanries753@gmail.com>
2025-04-08 22:16:05 +1200

    added the final content.

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-
+/*this makes a functions so that when the side bar is toggled use "active" this was for both title and sidebar*/
 function toggleSidebar() {
 	const sidebar = document.querySelector('.sidebar');
 	const title = document.querySelector('.title');
 	sidebar.classList.toggle('active');
 	title.classList.toggle('active');
 }
-
+/*this adds the fuctions for the slides, it allows for the buttons to comunicate to switch to the next slide, it also allows us to see if the user is on the last slide and if so he should get taken to the first/last slide.*/
 document.addEventListener("DOMContentLoaded", function() {
     let currentIndex = 0;
     const slides = document.getElementById("slides");
